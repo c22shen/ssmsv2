ea8075844f40aac9dcf8a90ca1da70fcb1c77e3d
mgkwon <mgskwon@gmail.com>
2016-01-12 17:00:11 -0500

    mqtt login info to env

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 var config = {};
 
 config.mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/ssms';
-config.yunUser=process.env.Yun_User;
-config.yunPass=process.env.Yun_Pass;
+config.yunUser=process.env.YUN_USER;
+config.yunPass=process.env.YUN_PASS;
+config.mqttUser=process.env.MQTT_USER;
+config.mqttPass=process.env.MQTT_PASS;
 module.exports = config;

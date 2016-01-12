7c2871e855971152acfdf9f11ba8eaa8e53eb273
mgkwon <mgskwon@gmail.com>
2016-01-12 17:05:10 -0500

    YUN_ to GMAIL_

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {};
 
 config.mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/ssms';
-config.yunUser=process.env.YUN_USER;
-config.yunPass=process.env.YUN_PASS;
+config.yunUser=process.env.GMAIL_USER;
+config.yunPass=process.env.GMAIL_PASS;
 config.mqttUser=process.env.MQTT_USER;
 config.mqttPass=process.env.MQTT_PASS;
 module.exports = config;

cb4f2d613c0ad7aaab0dec6df0da9933b1082dad
Xiao Shen <xiaoshen2009@gmail.com>
2016-01-19 14:50:31 -0500

    chart update

--- public/js/angular-app/linechart.js
+++ public/js/angular-app/linechart.js
@@ -24,12 +24,30 @@ angular
                     "sale": "176",
                     "year": "2010"
                 }];
-
+var data2 = [{
+    "sale": "152",
+    "year": "2000"
+}, {
+    "sale": "189",
+    "year": "2002"
+}, {
+    "sale": "179",
+    "year": "2004"
+}, {
+    "sale": "199",
+    "year": "2006"
+}, {
+    "sale": "134",
+    "year": "2008"
+}, {
+    "sale": "176",
+    "year": "2010"
+}];
 
                 var width = 1000,
                     height = 500;
 
-                var MARGINS = {
+                var margins = {
                     top: 20,
                     right: 20,
                     bottom: 20,
@@ -43,23 +61,58 @@ angular
                 var svg_container = d3.select(el).append('svg');
 
 
-                var xScale = d3.scale.linear().range([MARGINS.left, width - MARGINS.right]).domain([2000, 2010]);
-                var yScale = d3.scale.linear().range([height - MARGINS.top, MARGINS.bottom]).domain([134, 215]);
+                var xScale = d3.scale.linear().range([margins.left, width - margins.right]).domain([2000, 2010]);
+                var yScale = d3.scale.linear().range([height - margins.top, margins.bottom]).domain([134, 215]);
 
 
-                // xAxis = d3.svg.axis()
-                //     .scale(xScale);
+                xAxis = d3.svg.axis()
+                    .scale(xScale);
 
-                // yAxis = d3.svg.axis()
-                //     .scale(yScale);
+                yAxis = d3.svg.axis()
+                    .scale(yScale);
 
-                // var svg = svg_container
-                //     .attr('width', width)
-                //     .attr('height', height)
-                //     .append("g");
+                var svg = svg_container
+                    .attr('width', width)
+                    .attr('height', height)
+                    .append("g");
 
-                // svg.append("svg:g").call(xAxis);
+svg.append("svg:g")
+.attr("class","axis")
+    .attr("transform", "translate(0," + (height - margins.bottom) + ")")
+    .call(xAxis);
 
+
+    yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+    svg.append("svg:g")
+    .attr("class","axis")
+    // .attr("transform", "translate(" + (margins.left) + ",0)")
+    .call(yAxis);
+
+
+var lineGen = d3.svg.line()
+  .x(function(d) {
+    return xScale(d.year);
+  })
+  .y(function(d) {
+    return yScale(d.sale);
+  }).interpolate("basis");
+
+  svg.append('svg:path')
+  .attr('d', lineGen(data))
+  .attr('stroke', 'green')
+  .attr('stroke-width', 2)
+  .attr('fill', 'none');
+
+
+
+svg.append('svg:path')
+  .attr('d', lineGen(data2))
+  .attr('stroke', 'blue')
+  .attr('stroke-width', 2)
+  .attr('fill', 'none');
             }
             return {
                 link: link,
